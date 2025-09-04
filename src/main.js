83c6041f72216f32d3cdff8f3f22c3cf826a3a5a
Amir-Yadavar <amiresya@gmail.com>
2025-09-04 15:21:59 +0330

    create line and dash line in asphalt ..

--- src/main.js
+++ src/main.js
@@ -12,160 +12,103 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.z = -15;
-camera.position.y = 8;
-camera.position.x = 0;
-const gridHelper = new THREE.GridHelper(15, 15, 0xfff);
+camera.position.set(-2, 15, -30);
 
-// cube or object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-// meshBasicMaterial
-// const material = new THREE.MeshBasicMaterial({color:0x0000ff})
+// renderer
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-// meshPhongMaterial
-// const material = new THREE.MeshPhongMaterial({
-//   color: 0x0000ff,
-//   specular: 0xffffff,
-//   shininess: 200,
-// });
+// gltf
+const gltfLoader = new GLTFLoader();
+
+const loadModel = (url) => {
+  return new Promise((resolve, reject) => {
+    gltfLoader.load(
+      url,
+      (gltf) => resolve(gltf.scene),
+      undefined,
+      (error) => reject(error)
+    );
+  });
+};
+
+const saratogaGLTF = async () => {
+  const saratogaModel = await loadModel(
+    "./models/car/chrysler_saratoga_1960.glb"
+  );
+  saratogaModel.scale.set(0.01, 0.01, 0.01);
+  saratogaModel.position.set(3, 0, 0);
+  saratogaModel.rotation.y = Math.PI;
+
+  scene.add(saratogaModel);
+};
+const mercedesGLTF = async () => {
+  const mercedesModel = await loadModel(
+    "./models/car/mercedes-benz_slr_mclaren_2005.glb"
+  );
+  mercedesModel.scale.set(0.01, 0.01, 0.01);
+  mercedesModel.position.set(0, 0, 0);
+  mercedesModel.rotation.y = Math.PI / 2;
+
+  scene.add(mercedesModel);
+};
+
+mercedesGLTF();
+saratogaGLTF();
+
+// orbit controlls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.05;
 
 // move camera
-const speed = 0.1;
 
-document.addEventListener("keydown", (e) => {
+const speed = 0.2;
+
+window.addEventListener("keydown", (e) => {
   const forward = new THREE.Vector3();
   const right = new THREE.Vector3();
 
   camera.getWorldDirection(forward);
+  // forward.y = 0
+  forward.normalize();
 
-  right.crossVectors(forward, camera.up);
+  right.crossVectors(forward, camera.up).normalize();
 
-  switch (e.key) {
+  let moveVector = new THREE.Vector3();
+
+  switch (e.key.toLowerCase()) {
     case "w":
-      {
-        camera.position.addScaledVector(forward, speed);
-        controls.target.addScaledVector(forward, speed);
-      }
+      moveVector.copy(forward).multiplyScalar(speed);
       break;
     case "s":
-      {
-        camera.position.addScaledVector(forward, -speed);
-        controls.target.addScaledVector(forward, -speed);
-      }
+      moveVector.copy(forward).multiplyScalar(-speed);
       break;
     case "d":
-      {
-        camera.position.addScaledVector(right, speed);
-        controls.target.addScaledVector(right, speed);
-      }
+      moveVector.copy(right).multiplyScalar(speed);
       break;
     case "a":
-      {
-        camera.position.addScaledVector(right, -speed);
-        controls.target.addScaledVector(right, -speed);
-      }
+      moveVector.copy(right).multiplyScalar(-speed);
       break;
-
+    case "q":
+      moveVector.copy(camera.up).multiplyScalar(-speed);
+      break;
+    case "e":
+      moveVector.copy(camera.up).multiplyScalar(speed);
+      break;
     default:
-      break;
-  }
-});
-
-// meshStandardMaterial
-const material = new THREE.MeshStandardMaterial({
-  color: 0x44aa88,
-  metalness: 0.8,
-  roughness: 0.2,
-});
-const cube = new THREE.Mesh(geometry, material);
-cube.position.y = 1;
-// scene.add(cube);
-
-// texture
-const textureLoader = new THREE.TextureLoader();
-const woodGrayTexture = textureLoader.load(
-  "/img/texture/Wood034_1K-JPG_Color.jpg"
-);
-const woodGrayRoughnessTexture = textureLoader.load(
-  "/img/texture/Wood034_1K-JPG_Roughness.jpg"
-);
-const woodGrayNormalTexture = textureLoader.load(
-  "/img/texture/Wood034_1K-JPG_NormalDX.jpg"
-);
-const meatTexture = textureLoader.load("/img/texture/meat.jpg");
-
-// 3D models
-const gltfLoader = new GLTFLoader();
-gltfLoader.load("./models/burger.glb", (gltf) => {
-  const orginalModel = gltf.scene;
-
-  function createBurger(position, rotation) {
-    const model = orginalModel.clone();
-    model.position.copy(position);
-    model.rotation.y = rotation;
-
-    return model;
+      return;
   }
 
-  const burger1 = createBurger(new THREE.Vector3(0, 0, 0), Math.PI / 2);
-  const burger2 = createBurger(new THREE.Vector3(2.5, 0, -2.2), Math.PI);
-  const burger3 = createBurger(new THREE.Vector3(-2.3, 0, -2.2), Math.PI);
-  scene.add(burger1, burger2, burger3);
-
-  // model.traverse((child) => {
-  //   if (child.name === "Cylinder_3") {
-  //     child.material = new THREE.MeshStandardMaterial({
-  //       map: meatTexture,
-  //       // roughness: woodGrayRoughnessTexture,
-  //       normalMap: woodGrayNormalTexture,
-  //     });
-  //   }
-  // });
+  camera.position.add(moveVector);
 });
 
-// RayCaster
+// click on model
 
 const raycaster = new THREE.Raycaster();
-const mouse = new THREE.Vector2();
 
-const clickableSwitch = [];
-let lampRoof = null;
-let isLightOn = true;
-
-// light switch
-gltfLoader.load("./models/light_switch.glb", (gltf) => {
-  const model = gltf.scene;
-  model.scale.set(4, 4, 4);
-  model.rotateY(Math.PI);
-  model.position.set(0, 2, 9.9);
-  scene.add(model);
-
-  model.traverse((child) => {
-    if (child.name === "light_switch") {
-      clickableSwitch.push(child);
-      child.material = new THREE.MeshStandardMaterial({
-        color: 0xffffff,
-      });
-    }
-  });
-});
-
-gltfLoader.load("./models/Lamp_CIrcle.glb", (gltf) => {
-  const model = gltf.scene;
-  model.position.set(0, 3, 0);
-  scene.add(model);
-
-  model.traverse((child) => {
-    // console.log(child);
-    if (child.name === "Cylinder_1") {
-      lampRoof = child;
-      child.material = new THREE.MeshStandardMaterial({
-        color: 0xffffff,
-        
-      });
-    }
-  });
-});
+const mouse = new THREE.Vector3();
 
 window.addEventListener("click", (e) => {
   mouse.x = (e.clientX / window.innerWidth) * 2 - 1;
@@ -174,129 +117,168 @@ window.addEventListener("click", (e) => {
   raycaster.setFromCamera(mouse, camera);
 
   const hits = raycaster.intersectObjects(scene.children, true);
-  const switchHit = hits.find((h) => h.object.name === "light_switch");
-
-  if (switchHit) {
-    isLightOn = !isLightOn;
-    directionalLight.visible = isLightOn;
-
-    if (!lampRoof) {
-      console.warn("lampRoof هنوز مقدار نگرفته!");
-      return; // جلوتر نرو
-    }
-    if (isLightOn) {
-      lampRoof.material.color.set(0xffffff);
-    } else {
-      lampRoof.material.color.set(0x000000);
-    }
-    lampRoof.material.needsUpdate = true;
-  }
+  console.log(hits);
 });
 
-// create wall
-const wallHeight = 3;
-const wallGeometry = new THREE.BoxGeometry(20, wallHeight, 0.2);
-const wallMaterial = new THREE.MeshStandardMaterial({
-  color: 0xcccccc,
-  roughness: 2,
-});
+// texture
 
-const frontWall = new THREE.Mesh(wallGeometry, wallMaterial);
-frontWall.position.set(0, wallHeight / 2, 10);
+const textureLoader = new THREE.TextureLoader();
+const asphaltTexture = textureLoader.load(
+  "./img/texture/Asphalt/Asphalt026C_1K-JPG_Color.jpg"
+);
+const asphaltRoughTexture = textureLoader.load(
+  "./img/texture/Asphalt/Asphalt026C_1K-JPG_Roughness.jpg"
+);
+const asphaltNormalTexture = textureLoader.load(
+  "./img/texture/Asphalt/Asphalt026C_1K-JPG_NormalDX.jpg"
+);
 
-const backWall = new THREE.Mesh(wallGeometry, wallMaterial);
-backWall.position.set(0, wallHeight / 2, -10);
+// access texture for repeat
+asphaltTexture.wrapS = asphaltTexture.wrapT = THREE.RepeatWrapping;
+asphaltRoughTexture.wrapS = asphaltRoughTexture.wrapT = THREE.RepeatWrapping;
+asphaltNormalTexture.wrapS = asphaltNormalTexture.wrapT = THREE.RepeatWrapping;
+// repeat texture
+asphaltTexture.repeat.set(6, 6); // تعداد تکرار رنگ
+asphaltRoughTexture.repeat.set(6, 6); // باید با map هم‌تراز باشه
+asphaltNormalTexture.repeat.set(6, 6);
 
-const rightWall = new THREE.Mesh(wallGeometry, wallMaterial);
-rightWall.rotation.y = Math.PI / 2;
-rightWall.position.set(10, wallHeight / 2, 0);
+asphaltTexture.colorSpace = THREE.SRGBColorSpace;
 
-const leftWall = new THREE.Mesh(wallGeometry, wallMaterial);
-leftWall.rotation.y = Math.PI / 2;
-leftWall.position.s;
-leftWall.position.set(-10, wallHeight / 2, 0);
+const maxAniso = renderer.capabilities.getMaxAnisotropy();
+asphaltTexture.anisotropy = maxAniso;
+asphaltNormalTexture.anisotropy = maxAniso;
+asphaltRoughTexture.anisotropy = maxAniso;
 
-scene.add(frontWall, backWall, rightWall, leftWall);
+// create floor or planeGeometry
+const planeGeometry = new THREE.PlaneGeometry(30, 30);
+const planeMaterial = new THREE.MeshStandardMaterial({
+  map: asphaltTexture,
+  roughness: asphaltRoughTexture,
+  normalMap: asphaltNormalTexture,
 
-// create roof
-
-const roofGeometry = new THREE.PlaneGeometry(19.8, 19.8);
-// const roofMaterial = new THREE.MeshStandardMaterial({
-//   color: 0xcccccc,
-//   roughness: 2,
-//   side: THREE.DoubleSide,
-// });
-const roofMaterial = new THREE.MeshPhysicalMaterial({
-  color: 0xcccccc,
-  metalness: 0,
-  roughness: 0,
-  transmission: 1,
-  thickness: 0.5,
-  opacity: 2,
-  ior: 1, // ضریب شکست نور شیشه
-  clearcoat: 0,
-  clearcoatRoughness: 0,
   side: THREE.DoubleSide,
 });
-const roof = new THREE.Mesh(roofGeometry, roofMaterial);
-roof.rotation.x = -Math.PI / 2;
-roof.position.y = wallHeight;
-scene.add(roof);
-
-// floor geometry
-const floorGeometry = new THREE.PlaneGeometry(20, 20);
-const floorMaterial = new THREE.MeshStandardMaterial({
-  map: woodGrayTexture,
-  roughness: woodGrayRoughnessTexture,
-  normalMap: woodGrayNormalTexture,
-  side: THREE.DoubleSide,
-});
-
-const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+const floor = new THREE.Mesh(planeGeometry, planeMaterial);
 floor.rotation.x = -Math.PI / 2;
 scene.add(floor);
 
-// ambientLight
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
+// func for create solid line for avenue
+function createSolidLine(width, height, color) {
+  const geometry = new THREE.PlaneGeometry(width, height);
+  const materil = new THREE.MeshBasicMaterial({
+    color,
+    side: THREE.DoubleSide,
+  });
+
+  const solidLine = new THREE.Mesh(geometry, materil);
+  return solidLine;
+}
+
+const solidLine = createSolidLine(10, 0.2, 0xffffff);
+solidLine.rotation.x = Math.PI / 2;
+solidLine.position.y = 0.08;
+
+const bottomRightLine = solidLine.clone();
+bottomRightLine.rotation.z = Math.PI / 2;
+bottomRightLine.position.z = -10;
+bottomRightLine.position.x = -5;
+
+const bottomleftLine = solidLine.clone();
+bottomleftLine.rotation.z = Math.PI / 2;
+bottomleftLine.position.z = -10;
+bottomleftLine.position.x = 5;
+
+const upRightLine = solidLine.clone();
+upRightLine.rotation.z = Math.PI / 2;
+upRightLine.position.z = 10;
+upRightLine.position.x = -5;
+
+const upleftLine = solidLine.clone();
+upleftLine.rotation.z = Math.PI / 2;
+upleftLine.position.z = 10;
+upleftLine.position.x = 5;
+
+const rightUpLine = solidLine.clone();
+rightUpLine.position.x = -10;
+rightUpLine.position.z = 5;
+
+const rightBottomLine = solidLine.clone();
+rightBottomLine.position.x = -10;
+rightBottomLine.position.z = -5;
+
+const leftUpLine = solidLine.clone();
+leftUpLine.position.x = 10;
+leftUpLine.position.z = 5;
+
+const leftBottomLine = solidLine.clone();
+leftBottomLine.position.x = 10;
+leftBottomLine.position.z = -5;
+
+scene.add(
+  bottomRightLine,
+  bottomleftLine,
+  rightUpLine,
+  rightBottomLine,
+  upRightLine,
+  upleftLine,
+  leftBottomLine,
+  leftUpLine
+);
+
+function createDasheLine(width, height, color) {
+  const geometry = new THREE.PlaneGeometry(width, height);
+  const materil = new THREE.MeshBasicMaterial({
+    color,
+    side: THREE.DoubleSide,
+  });
+
+  const line = new THREE.Mesh(geometry, materil);
+  line.rotation.x = -Math.PI / 2; // بخوابه روی زمین
+  line.position.y = 0.01; // کمی بالاتر از زمین
+  return line;
+}
+// dash for bottom
+for (let z = -14.5; z < -5; z += 2) {
+  const dashBottom = createDasheLine(1, 0.4, 0xffffff);
+  dashBottom.rotation.z = Math.PI / 2;
+  dashBottom.position.z = z;
+  scene.add(dashBottom);
+}
+// dash for up
+for (let z = 14.5; z > 5; z -= 2) {
+  const dashUp = createDasheLine(1, 0.4, 0xffffff);
+  dashUp.rotation.z = Math.PI / 2;
+  dashUp.position.z = z;
+  scene.add(dashUp);
+}
+// dash for right
+for (let x = -14.5; x < -5; x += 2) {
+  const dashRight = createDasheLine(1, 0.4, 0xffffff);
+  dashRight.position.x = x;
+  scene.add(dashRight);
+}
+// dash for left
+for (let x = 14.5; x > 5; x -= 2) {
+  const dashleft = createDasheLine(1, 0.4, 0xffffff);
+  dashleft.position.x = x;
+  scene.add(dashleft);
+}
+
+// ambient light
+const ambientLight = new THREE.AmbientLight(0xffffff, 1);
 scene.add(ambientLight);
 
 // pointLight
-// const pointLight = new THREE.PointLight(0xffffff,2,0)
-// pointLight.position.set(2,2,2)
-// const pointLightHelper = new THREE.PointLightHelper(pointLight)
+const pointLight = new THREE.PointLight(0xffffff, 5);
+pointLight.position.set(2, 5, 2);
+const pointLightHelper = new THREE.PointLightHelper(pointLight);
 // scene.add(pointLight,pointLightHelper)
 
-// directionalLight
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-const directionalLightHelper = new THREE.DirectionalLightHelper(
-  directionalLight
-);
-directionalLight.position.set(0, 5, 0);
-scene.add(directionalLight);
-
-// spotLight
-// const spotLight = new THREE.SpotLight(0xffffff, 1, 0,Math.PI / 3);
-// const spotLightHelper = new THREE.SpotLightHelper(spotLight);
-// spotLight.position.set(10,20,10);
-// spotLight.target.position.set(0,0,0)
-// scene.add(spotLight,spotLight.target, spotLightHelper);
-
-// renderer
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-
-const controls = new OrbitControls(camera, renderer.domElement);
-
-controls.enableDamping = true;
-// controls.dampingFactor = 0.3
-
-document.body.appendChild(renderer.domElement);
+// animate func
 
 function animate() {
   requestAnimationFrame(animate);
-
   controls.update();
-
   renderer.render(scene, camera);
 }
 
